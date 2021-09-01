862a1e4b1124cc7894b56f8010a31675a5637828
DINESH359 <63196767+DINESH359@users.noreply.github.com>
2021-09-01 12:10:07 +0530

    Add files via upload

---
+++ 1_node.js
@@ -0,0 +1,7 @@
+const num = 12;
+if (num > 10) {
+    console.log("number is large");
+} else {
+    console.log("number is smaller");
+}
+console.log(`hey its my first node app`);
---
+++ app6.js
@@ -0,0 +1,17 @@
+var http = require('http')
+var fs = require('fs')
+
+http.createServer(function(req, res) {
+    if (req.url === '/') {
+        fs.createReadStream(__dirname + '/index.html').pipe(res);
+    }
+    if (req.url === '/api') {
+        res.writeHead(200, { 'Content-Type': 'application/json' })
+
+        var obj = {
+            first: 'Dinesh',
+            last: 'Jha'
+        }
+        res.end(JSON.stringify(obj));
+    }
+}).listen(1337, '127.0.0.1');
